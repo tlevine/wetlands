37ad6337849d36e7f2e1848b9acdd40c21ff784f
Thomas Levine <occurrence@thomaslevine.com>
2012-05-29 02:04:06 -0400

    flatten

--- flatten.sql
+++ flatten.sql
@@ -1,20 +1,20 @@
 -- Generate flat CSV files
--- select count(*), Listing.scraper_run, ListingData.[PermitApplication No.] from Listing JOIN ListingData ON Listing.scraper_run = ListingData.scraper_run AND Listing.kwargs=ListingData.kwargs join [Public Notice] ON Listing.scraper_run = [Public Notice].scraper_run AND Listing.kwargs = [Public Notice].motherkwargs JOIN [Public Notice Download] on [Public Notice].kwargs = [Public Notice Download].kwargs AND [Public Notice].kwargs=[Public Notice Download].kwargs order by ListingData.[PermitApplication No.] limit 30;
+.mode csv
+.header on
 
-select count(*) from [Public Notice];
-
-select count(*)
-from
+select * from
   [Public Notice]
   -- Add rows
-  JOIN Listing on Listing.scraper_run = [Public Notice].scraper_run AND Listing.kwargs=[Public Notice].motherkwargs
+  JOIN Listing ON
+    [Public Notice].scraper_run =Listing.scraper_run AND
+    [Public Notice].motherkwargs = Listing.kwargs
 
   -- Add columns
   JOIN [Public Notice Download] ON
    [Public Notice].scraper_run = [Public Notice Download].scraper_run AND
    [Public Notice].kwargs=[Public Notice Download].kwargs
---  JOIN ListingData ON
-  --  Listing.scraper_run = ListingData.scraper_run AND
-    --Listing.kwargs=ListingData.kwargs
+  JOIN ListingData ON
+    [Public Notice Download].scraper_run = ListingData.scraper_run AND
+    [Public Notice Download].[PermitApplication No.]=ListingData.[PermitApplication No.]
 --Listing.scraper_run = ListingData.scraper_run AND Listing.kwargs=ListingData.kwargs
-;
+limit 1;
