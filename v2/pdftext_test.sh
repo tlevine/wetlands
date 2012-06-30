4b7ab158612038d575cf5e3b3d69749c408ce504
Thomas Levine <occurrence@thomaslevine.com>
2012-06-30 12:24:18 -0400

    ??

---
+++ v2/pdftext_test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+file=fixtures/Kilbride\ PN.pdf
+
+setup () {
+  rm -f "$file.txt"
+  ./pdftext "$file"
+}
+
+runtests() {
+  text="ARMY"
+  assert 'The text file is created in the right place' [ -e fixtures/Kilbride\ PN.pdf.txt ]
+  assert "The text file contains the text \"$text\"" \
+    [ "`grep -c \"$text\" fixtures/Kilbride\ PN.pdf.txt`" != '0' ]
+}
+
+#teardown () {
+#  rm -f "$file.txt"
+#}
