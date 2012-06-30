d5f212d7cd84b2f89c0db38cf3b7a7a716724cdf
Thomas Levine <occurrence@thomaslevine.com>
2012-06-30 20:24:09 +0100

    aoeu

--- v2/papers_test/parameters.sh
+++ v2/papers_test/parameters.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-. papers.sh
+. ./papers.sh
 
 runtests() {
-  assert 'paper function errors when type is not public_notice or drawing' false
-  assert 'paper function passes when type is public_notice' false
-  assert 'paper function passes when type is drawing' false
+  assert 'paper function errors when type is not public_notice or drawing'\
+    'paper --papertype=invalid --date=2012-04-01 --url=http://localhost:9001 --permit=MVN-foo-bar-baz'
+# assert 'paper function passes when type is public_notice' false
+# assert 'paper function passes when type is drawing' false
 }
