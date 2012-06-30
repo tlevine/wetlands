d362b0d3eb6d577b9efdaaccec14e0dfc943e7b8
Thomas Levine <occurrence@thomaslevine.com>
2012-06-30 17:39:25 +0100

    text in test

--- v2/ocr_test.sh
+++ v2/ocr_test.sh
@@ -8,7 +8,7 @@ setup () {
 }
 
 runtests() {
-  text=aoeuaoeuoaeu
+  text="ARMY"
   assert 'The text file is created in the right place' [ -e "$file".txt ]
   assert "The text file contains the text \"$text\"" [ "`grep -c $text $file.txt`" != '0' ]
 }
