0c05e4826f0d1e7f3710ada3941a7a676d47487e
Thomas Levine <occurrence@thomaslevine.com>
2012-06-30 13:55:25 +0100

    test paper download

---
+++ v2/papers_test/download_one.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# The server.example directory must be served at localhost:5678
+# The pdf directory must be empty.
+
+. papers.sh
+
+
+runtests() {
+  permit=MVN-a93409234
+  date=2012-06-01
+  paper
+    --type public_notice \
+    --url localhost:5678/thuho.pdf \
+    --permit $permit \
+    --date $date
+  assert 'Something has been saved' [ -e pdfs/$permit/public_notice-$date.pdf ]
+  assert 'Something md5sum has been created' [ -e pdfs/$permit/public_notice-$date.pdf.md5 ]
+  assert 'md5sum matches' false
+  assert 'The appropriate short link has been created' false
+  assert 'paper errors when type is not public_notice or drawing' false
+}
