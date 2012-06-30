895f9a5edb2b03522cd9b5cae3603bdc24277dcf
Thomas Levine <occurrence@thomaslevine.com>
2012-06-30 20:00:11 +0100

    download_many test

--- v2/papers_test/download_many.sh
+++ v2/papers_test/download_many.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-. papers.sh
+. ./papers.sh
 
 runtests() {
   permit=MVN-a93409234
-  paper
+  paper \
     --papertype public_notice \
     --url localhost:5678/thuho.pdf \
     --permit $permit \
     --date 2012-05-28
-  paper
+  paper \
     --papertype public_notice \
     --url localhost:5678/thuho.pdf \
     --permit $permit \
@@ -19,5 +19,5 @@ runtests() {
   assert 'Something has been saved for June 1' [ -e pdfs/$permit/public_notice-2012-06-01.pdf ]
   assert 'md5sum has been saved for May 28' [ -e pdfs/$permit/public_notice-2012-05-28.pdf.md5 ]
   assert 'md5sum has been saved for June 1' [ -e pdfs/$permit/public_notice-2012-06-01.pdf.md5 ]
-  assert 'The appropriate short link has been created' false
+  assert 'The appropriate short link has been created' [ "`readlink pdfs/$permit/public_notice.pdf`" = "pdfs/$permit/public_notice-2012-06-01.pdf" ]
 }
