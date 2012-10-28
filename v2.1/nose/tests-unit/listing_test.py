7e416ccbeb80a6f0f832c41ecedbe65c8afff9c1
Thomas Levine <occurrence@thomaslevine.com>
2012-10-28 11:15:55 -0400

    copy v2 parser and unit-tests

---
+++ v2.1/nose/tests-unit/listing_test.py
@@ -0,0 +1,129 @@
+import os
+import unittest
+import datetime
+import json
+from listing import listing_parse, \
+    _clean_permit_application_number, \
+    _clean_mvn_permit_application_number
+
+class TestListingParse(unittest.TestCase):
+    def setUp(self):
+        "Load HTML and expected JSON and parse the HTML."
+        listing = open(os.path.join('fixtures', 'listing.html'))
+        listingData = open(os.path.join('fixtures', 'listing.json'))
+
+        self.html = listing.read()
+        self.observedData = listing_parse(self.html)
+        self.expectedData = json.loads(listingData.read())
+
+        listing.close()
+        listingData.close()
+
+    def test_length(self):
+        "The table should have 44 items."
+        self.assertEqual(len(self.observedData), 44)
+
+    def test_types(self):
+        "The data should be of appropriate types."
+        expectedTypes = {
+            'projectDescription': unicode,
+            'applicant': unicode,
+            'location': unicode,
+            'publicNoticeDate': datetime.datetime,
+            'expirationDate': datetime.datetime,
+            'permitApplicationNumber': unicode,
+            'publicNoticeUrl': unicode,
+            'drawingsUrl': unicode,
+            'projectManagerEmail': unicode,
+            'projectManagerName': unicode,
+            'projectManagerPhone': unicode,
+        }
+        self.maxDiff = None
+        observedTypes = {k: type(v) for k, v in self.observedData[0].items()}
+        self.assertDictEqual(observedTypes, expectedTypes)
+
+    def test_keys(self):
+        "The data should have the appropriate keys."
+        observed = set(self.observedData[0].keys())
+        expected = {
+#           'scriptRuns', # List of datetimes when run
+            'location',
+            'projectDescription',
+            'applicant',
+            'publicNoticeDate',
+            'expirationDate',
+            'permitApplicationNumber',
+            'publicNoticeUrl',
+            'drawingsUrl',
+            'projectManagerEmail',
+            'projectManagerName',
+            'projectManagerPhone',
+        }
+        self.assertSetEqual(observed, expected)
+
+class TestMVNPermitApplicationNumberConversion(unittest.TestCase):
+    'Permit application numbers should get cleaned up, or I should see errors.'
+
+    def _p(self, permit_application_number, should = None):
+        'Does the cleaning happen properly?'
+
+        if should == None:
+            should = permit_application_number
+
+        self.assertEqual(
+            _clean_mvn_permit_application_number(permit_application_number),
+            should
+        )
+
+    def _r(self, permit_application_number, raises = AssertionError):
+        'Does the cleaning raise an error?'
+        with self.assertRaises(raises):
+            _clean_mvn_permit_application_number(permit_application_number)
+
+    def test_clean_short(self):
+        self._p("MVN-2012-00926")
+
+    def test_clean_long(self):
+        self._p("MVN-2012-01027-WMM")
+
+    def test_space1_long(self):
+        self._p("MVN 2010-1270-WII", should = "MVN-2010-1270-WII")
+
+    def test_space2_long(self):
+        self._p("MVN-2010 1270-WII", should = "MVN-2010-1270-WII")
+
+    def test_space3_long(self):
+        self._p("MVN-2010-1270 WII", should = "MVN-2010-1270-WII")
+
+    def test_space1_space3_long(self):
+        self._p("MVN 2010-1270 WII", should = "MVN-2010-1270-WII")
+
+    def test_weirdspace_space1_long(self):
+        self._p("MVN 2006-0335-CY A", should = "MVN-2006-0335-CYA")
+
+    def test_weirdspace_long(self):
+        self._p("MVN-2006-0335-CY A", should = "MVN-2006-0335-CYA")
+
+    def test_bad_first_block_long(self):
+        self._r("ABC-2012-01027-WMM")
+
+    def test_old_year_second_block(self):
+        self._p("MVN-1987-01027-WMM")
+
+    def test_strange_year_second_block(self):
+        self._r("MVN-1887-01027-WMM")
+
+    def test_nonletters_in_fourth_block(self):
+        self._r("MVN-2012-01027-W3M")
+
+    def test_fourth_block_length(self):
+        self._p("MVN-2012-0000-ABCD")
+
+    def test_second_block_length(self):
+        self._r("MVN-02012-0000-ABC")
+
+    def test_short_fourth_block(self):
+        self._p('MVN-2012-01053-CJ')
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ v2.1/src/listing.py
@@ -0,0 +1,207 @@
+import datetime
+import lxml.html, lxml.etree
+import re
+from unidecode import unidecode
+from urllib2 import urlopen
+
+def listing_parse(rawtext):
+    # There are more data in the comments!
+    text_with_locations = rawtext.replace('<!--', '').replace('-->', '').replace('&nbsp;', ' ')
+    unicodetext = unidecode(text_with_locations)
+    html = lxml.html.fromstring(unicodetext)
+    table = _onenode(html, '//table[@width="570" and @border="1" and @cellpadding="0" and @cellspacing="0" and @bordercolor="#ffffff" and @bgcolor="#efefef"]')
+    trs = table.xpath('tr')
+
+    # Getting the cells
+    thead = [td.text_content().strip() for td in trs.pop(0)]
+    if len(thead) != _NCOL:
+        _log(thead)
+        _RRRaise(AssertionError('The table header does not have exactly %d cells.' % _NCOL))
+
+    if thead != _COLNAMES:
+        pairs = zip(thead, _COLNAMES)
+        for a, b in pairs:
+            _log(a, b), 'Match' if a == b else 'Differ'
+        _RRRaise(AssertionError('The table header does not have the right names.'))
+
+    # List of dictionaries of data
+    data = []
+    publicnotices = []
+    drawings = []
+    for tr in trs:
+        if len(tr.xpath('td')) != _NCOL:
+            _RRRaise(AssertionError('The table row does not have exactly %d cells.' % _NCOL))
+
+        # As a dict
+        row = dict(zip(thead, [td.text_content().strip() for td in tr.xpath('td')]))
+
+        # Clean up the permit application number
+        row['PermitApplication No.'] = _clean_permit_application_number(row['PermitApplication No.'])
+
+        # Dates
+        row['Public Notice Date'] = _parsedate(row['Public Notice Date'])
+        row['Expiration Date'] = _parsedate(row['Expiration Date'])
+
+        # PDF download links
+        del(row['View or Download'])
+        pdfkeys = set(tr.xpath('td[position()=6]/descendant::a/text()'))
+        if not pdfkeys.issubset({'Public Notice', 'Drawings'}):
+            _log(pdfkeys)
+            _RRRaise(AssertionError('The table row has unexpected hyperlinks.'))
+        if len(pdfkeys) == 0:
+            print row
+            _RRRaise(AssertionError('No pdf hyperlinks found for permit %s.' % row['PermitApplication No.']))
+        for key in ['Public Notice', 'Drawings']:
+            try:
+                row[key] = _onenode(tr, 'td/descendant::a[text()="%s"]/@href' % key)
+            except AssertionError:
+                if key == 'Public Notice':
+                    raise
+                else:
+                    continue
+
+            if row[key][:4] != 'pdf/':
+                _RRRaise(AssertionError('The %s pdf link doesn\'t have the expected path.' % key))
+
+        # Project manager contact information
+        del(row['Project Manager'])
+        pm = _onenode(tr, 'td[position()=8]')
+
+        # Email address
+        try:
+            row['Project Manager Email'] = _onenode(pm, 'descendant::a/@href')
+        except AssertionError:
+            _log(row)
+            raise
+
+        if row['Project Manager Email'][:7] == 'mailto:':
+            row['Project Manager Email'] = unicode(row['Project Manager Email'][7:])
+        else:
+            msg = 'This is a strange email link: <%s>' % row['Project Manager Email']
+            _RRRaise(AssertionError(msg))
+
+        # Name
+        row['Project Manager Name'] = _onenode(pm, 'descendant::a').text_content().strip()
+
+        # Phone number
+        phone_match = re.match(_PHONE_NUMBER, pm.text_content())
+        if phone_match:
+            row['Project Manager Phone'] = phone_match.group(1)
+        else:
+            _log(row)
+            msg = 'This is a strange phone number: %s' % pm.text_content()
+            _RRRaise(AssertionError(msg))
+
+        # Append to our big lists
+        data.append(row)
+
+    data2 = []
+    for row in data:
+        row2 = {new: row.get(old, None) for old, new in _KEYMAP}
+        for k, v in row2.items():
+            if type(v) in {lxml.etree._ElementStringResult, str}:
+                row2[k] = unicode(v)
+        data2.append(row2)
+
+    return data2
+
+_KEYMAP = [
+    ('Project Description','projectDescription'),
+    ('Applicant','applicant'),
+    ('PermitApplication No.','permitApplicationNumber'),
+    ('Public Notice Date','publicNoticeDate'),
+    ('Public Notice','publicNoticeUrl'),
+    ('Location','location'),
+    ('Drawings','drawingsUrl'),
+    ('Expiration Date','expirationDate'),
+    ('Project Manager Email', 'projectManagerEmail'),
+    ('Project Manager Name','projectManagerName'),
+    ('Project Manager Phone','projectManagerPhone'),
+]
+_PHONE_NUMBER = re.compile(r'[^0-9]*(\d{3}-\d{3}-\d{4})[^0-9]*')
+_NCOL = 8
+_COLNAMES = [
+    'Project Description',
+    'Applicant',
+    'Public Notice Date',
+    'Expiration Date',
+    'PermitApplication No.',
+    'View or Download',
+    'Location',
+    'Project Manager'
+]
+
+PERMIT_APPLICATION_NUMBER_REGEX = re.compile(r'^MVN-[0-9]+-[0-9]+(?:-[A-Z]+)?$')
+PERMIT_YEAR = re.compile(r'[12][901][789012][0-9]')
+
+MANUAL_REPLACEMENTS = {
+    'MVN 2009-3063 CO (ERRATUM)': 'MVN-2009-3063-CO-(ERRATUM)',
+    'MVN 2010-1080 WLL/ MVN 2010 1032 WLL B': 'MVN-2010-1080-WLL_MVN-2010-1032-WLLB',
+    'MVN-2010-1080-WLL/ MVN-2010-1032-WLL-A': 'MVN-2010-1080-WLL_MVN-2010-1032-WLL-A',
+}
+
+def _parsedate(rawdate):
+    return datetime.datetime.strptime(rawdate, '%m/%d/%Y')
+
+def _clean_permit_application_number(n):
+    'Clean up the permit application number.'
+    if n[:3] == 'MVN':
+        return _clean_mvn_permit_application_number(n)
+    elif n[:3] == 'CEM':
+        return _clean_cem_permit_application_number(n)
+    else:
+        raise AssertionError('Unexpected first block in %s' % n)
+
+def _clean_cem_permit_application_number(n):
+    'Clean up the permit application number for CEM permits.'
+    for c in '/ \\':
+        assert c not in n, n
+    assert n.upper() == n
+    return n
+
+def _clean_mvn_permit_application_number(n):
+    'Clean up the permit application number for MVN permits.'
+
+    # If this is a manual one, replace it that way.
+    if n in MANUAL_REPLACEMENTS:
+        return MANUAL_REPLACEMENTS[n]
+
+    # Remove delimiters
+    n = filter(lambda char: char not in '- ', n)
+
+    # Add hyphen delimeters
+    n = n[:3] + '-' + n[3:7] + '-' + n[7:]
+
+    # If there's a fourth group
+    if re.match(r'.+[0-9][A-Z]+$', n):
+        # Add the delimiter
+        n = re.sub(r'(.+[0-9])([A-Z]+)$', r'\1-\2', n)
+    else:
+        if not re.match(r'.+-[0-9]+$', n):
+            raise AssertionError('The third group of %s is not all numbers.' % n)
+
+    # Check year
+    if 'MVN' != n[:3]:
+        raise AssertionError('The first three letters of %s are not "MVN"' % n)
+
+    # Check year
+    if not re.match(PERMIT_YEAR, n[4:8]):
+        raise AssertionError('The second group of %s doesn\'t seem like a year.' % n)
+
+    # Final check
+    if not re.match(PERMIT_APPLICATION_NUMBER_REGEX, n):
+        raise AssertionError(
+            'Permit application number %s could not be cleaned up.' % n
+        )
+
+    return n
+
+def _onenode(html, xpath):
+    nodes = html.xpath(xpath)
+    if len(nodes) != 1:
+        raise AssertionError('Not exactly one node')
+    else:
+        return nodes[0]
+
+if __name__ == '__main__':
+    main()
