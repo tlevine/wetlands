bddc19922172174d4dc98f9793f6b1deed58d7d0
Thomas Levine <occurrence@thomaslevine.com>
2013-01-13 14:44:41 -0500

    add pdf building

---
+++ v2.1/nose/tests-unit/test_pdf.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python2
+# -*- encoding: utf-8 -*-
+"""
+Test that the right information is extracted from each pdf
+
+Handling null values:
+  * No value means that the information has not been processed.
+  * None or the empty list indicates that the information is missing from the translated document.
+  * The empty string indicates that the value is "".
+"""
+
+import os
+import unittest
+from pdf import read_public_notice, \
+    _read_coords, _convert_coords, \
+    _read_wqc_number, \
+    _read_cup_number, \
+    _read_acres, \
+    _read_terms
+
+class TestAcres(unittest.TestCase):
+    def setUp(self):
+        self.rawtext = '''
+3): Lat 29° 12' 09.91"N / Long -89° 38' 36.53"W; Borrow Area 9 (Pt. 9):
+Lat 29° 12' 52.5"N / Long -89° 36' 49.7"W; Section 39-41, T20S-R28E; Section 16-21 ,26-28,34,35,1 3, 24, T218-
+R28E; West of Empire Waten/vay within Bastian Bay area.
+DESCRIPTION: Shell Island East Restoration Berm Enhancement Project and Shell Island West NRDA Restoration Project (BA-
+110/11 1) forthe proposed creation of +/- 615 acres of beach/dune habitat and +/-343 acres of brackish marsh
+habitat along the Barataria Basin Barrier Shoreline. Project includes the dredging of +/-12.9 M c.y. of sediment from
+potential borrow areas MR-A, MR-B, MR-E, 35E, and 9 (MR-B and MR-E previously permitted under P20101416 -
+Scofield Island Restoration Project/ BA-40). Conveyance of sediment within a submerged/buried 30" pipeline
+proposed from borrow sources MR-A, MR-B, and MR-E along an existing dredge pipeline corridor follows the
+Empire Waterway (previously permitted under P20101416) and from Areas 35E and 9 with estimated losses in
+sediment due to transfer anticipated. Proposed till required for the creation of dune habitat is +/- 6.4M c.y. and +/-
+1.8M c.y. for the creation of marsh habitat. Project also includes the excavation of +/- 467,900 c.y. for access
+channels and subsequent placement on both sides to a height conducive to marsh establishment, excavation of +/-
+865,000 c.y. for the construction of the primary dikes for containment of marsh creation areas, the placement 250
+c.y. of gravel for dredge pipeline levee crossings, and the excavation of +/- 21,400 c.y. for navigational crossings,
+booster pump locations, Empire Harbor Canal crossing, Highway 11 and 23 crossings, and the Empire Marsh
+crossing. "Approximately 2,291 ac. of non-vegetated waterbottom, 1.2 ac. of non-wet, and 132 ac. of vegetated
+wetland habitat may be impacted as a result of proposed activity.**
+Approximately 1,832 acres of Essential Fish Habitat (EFH) utilized by various life stages of red drum and panaeid
+shrimp would be altered or destroyed by this project. This information is provided to initiate federal consultation
+requirements pertaining to EF H under the Magnuson-Stevens Fishery Conservation and Management Act.*
+NOTICE the Louisiana Department of Natural Resources, Office of Coastal Management (OCM) has
+received the above application for a Coastal Use Permit (CUP) in accordance with the State and Local
+Coastal Resources Management Act of 1978, as amended, (Louisiana R.S. 49:2'''
+    def test_go(self):
+        self.assertListEqual(_read_acres(self.rawtext), [615., 343., 1832.])
+
+class TestReadTerms(unittest.TestCase):
+    """
+		At least for normal, non-JPN permits
+--> optionally, if "mitigation bank" found, remark that.
+           --> optionally, if "drill" found, remark that.
+           --> optionally, if "road" found, remark that.
+"""
+    def test_1(self):
+        o = _read_terms('mitigation Bank section 404')
+        e = {
+            'Mitigation Bank': True,
+            'Drill': False,
+            'Road': False,
+            'Section 10': False,
+            'Section 404': True,
+        }
+        self.assertDictEqual(o, e)
+
+class TestCUP(unittest.TestCase):
+    def setUp(self):
+        self.rawtext = '''
+OFFICE OF COASTAL MANAGEMENT
+P.O. BOX 44487
+BATON ROUGE, LA 70804-4487
+Phone (225)342-6140
+Fax (225) 342-9439
+Email sharon.mccarthy@la.gov
+OCM REVIEWER:
+Sharon Mccarthy
+CUP NUMBER:
+P20120216
+Plaquemines Parish, LA; Shell Island East (Pt. 250): Lat 29° 16' 38.24"N / Long -89° 37' 43.91"W; Shell Island
+West (Pt. 240): Lat 29° 17' 49.45"N / Long -89° 39' 51 .73"W; Borrow Areas/MR-A (Pt. 3): Lat 29° 26' 57.94"N/
+Long -89° 36' 11.43"W; MR-B (Pt. 13): Lat 29° 22' 52.80"N / Long -89° 34' 36.95"W; MR-E (Pt. 6): Lat 29° 21'
+04.95"N / Long -89° 30' 07.18"W; 35E (Pt. 3): Lat 29° 12' 09.91"N / Long -89° 38' 36.53"W; Borrow Area 9 (Pt. 9):
+Lat 29° 12' 52.5"N / Long -89° 36' 49.7"W; Section 39-41, T20S-R28E; Section 16-21 ,26-28,34,35,1 3, 24, T218-
+R28E; West of Empire Waten/vay within Bastian Bay area.
+DESCRIPTION: Shell Island East Restoration Berm Enhancement Project and Shell Island West NRDA Restoration Project (BA-
+110/11 1) forthe proposed creation of +/- 615 acres of beach/dune habitat and +/-343 acres of brackish marsh
+habitat along the Barataria Basin Barrier Shoreline. Project includes the dredging of +/-12.9 M c.y. of sediment from
+potential borrow areas MR-A, MR-B, MR-E, 35E, and 9 (MR-B and MR-E previously permitted under P20101416 -
+Scofield Island Restoration Project/ BA-40). Conveyance of sediment within a submerged/buried 30" pipeline
+proposed from borrow sources MR-A, MR-B, and MR-E along an existing dredge pipeline corridor follows the
+Empire Waterway (previously permitted under P20101416) and from Areas 35E and 9 with estimated losses in
+sediment due to transfer anticipated. Proposed till required for the creation of dune habitat is +/- 6.4M c.y. and +/-
+1.8M c.y. for the creation of marsh habitat. Project also includes the excavation of +/- 467,900 c.y. for access
+channels and subsequent placement on both sides to a height conducive to marsh establishment, excavation of +/-
+865,000 c.y. for the construction of the primary dikes for containment of marsh creation areas, the placement 250
+c.y. of gravel for dredge pipeline levee crossings, and the excavation of +/- 21,400 c.y. for navigational crossings,
+booster pump locations, Empire Harbor Canal crossing, Highway 11 and 23 crossings, and the Empire Marsh
+crossing. "Approximately 2,291 ac. of non-vegetated waterbottom, 1.2 ac. of non-wet, and 132 ac. of vegetated
+wetland habitat may be impacted as a result of proposed activity.**
+'''
+    def test_go(self):
+        observed = _read_cup_number(self.rawtext)
+        expected = {'P20120216','P20101416'}
+        self.assertEqual(observed, expected)
+
+class TestWQC(unittest.TestCase):
+    def setUp(self):
+        self.rawtext = '''
+ttn: Water Quality Certifications
+Post Office Box 4313
+Baton Rouge, LA 70821-4313
+
+(504) 862-2675 FAX (504) 862-2574
+Project Manager
+Jamie Crowe
+Permit Application Number
+MVN 1999-3891 CO
+
+(225) 219-3225 FAX (225) 325-8250
+Project Manager
+Jamie Phillippe
+WQC Application Number
+990825-04
+
+Interested parties are hereby notified that a permit application has been received by the New
+Orleans District of the U.S. Army Corps of Engineers pursuant to: [X] Section 10 of the Rivers and
+Harbors Act of March 3, 1899 (30 Stat. 1151; 33 USC 403); and/or [X] Section 404 of the Clean Water
+Act (86 Stat. 816; 33 USC 1344).
+Application has also been made to the Louisiana Department of Environmental Quality,
+Office of Environmental Services, for a Water Quality Certification (WQC) in accordance with statutory
+authority contained in LRS 30:2074 A(3) and provisions of Section 401 of the Clean Water Act.
+MODIFICATION OF SHIP MOORING FACILITY IN
+'''
+    def test_go(self):
+       self.assertEqual(_read_wqc_number(self.rawtext), '990825-04')
+
+class TestConvertCoords(unittest.TestCase):
+    def setUp(self):
+        self.rawtext = '''
+Plaquemines Parish, LA; Shell Island East (Pt. 250): Lat 29° 16' 38.24"N / Long -89° 37' 43.91"W; Shell Island
+West (Pt. 240): Lat 29° 17' 49.45"N / Long -89° 39' 51 .73"W; Borrow Areas/MR-A (Pt. 3): Lat 29° 26' 57.94"N/
+Long -89° 36' 11.43"W; MR-B (Pt. 13): Lat 29° 22' 52.80"N / Long -89° 34' 36.95"W; MR-E (Pt. 6): Lat 29° 21'
+04.95"N / Long -89° 30' 07.18"W; 35E (Pt. 3): Lat 29° 12' 09.91"N / Long -89° 38' 36.53"W; Borrow Area 9 (Pt. 9):
+Lat 29° 12' 52.5"N / Long -89° 36' 49.7"W; Section 39-41, T20S-R28E; Section 16-21 ,26-28,34,35,1 3, 24, T218-
+R28E; West of Empire Waten/vay within Bastian Bay area.
+'''
+    def test_minutes_seconds(self):
+        self.maxDiff = None
+        observed = _read_coords(self.rawtext, decimal = False)
+        expected = [
+            # Latitude, longitude
+            ( ( 29.0, 16.0, 38.24 ) , ( -89.0, -37.0, -43.91 ) ),
+            ( ( 29.0, 17.0, 49.45 ) , ( -89.0, -39.0, -51.73 ) ),
+            ( ( 29.0, 26.0, 57.94 ) , ( -89.0, -36.0, -11.43 ) ),
+            ( ( 29.0, 22.0, 52.80 ) , ( -89.0, -34.0, -36.95 ) ),
+            ( ( 29.0, 21.0, 04.95 ) , ( -89.0, -30.0, -07.18 ) ),
+            ( ( 29.0, 12.0, 09.91 ) , ( -89.0, -38.0, -36.53 ) ),
+            ( ( 29.0, 12.0, 52.5  ) , ( -89.0, -36.0, -49.7  ) ),
+        ]
+        self.assertListEqual(observed, expected)
+
+    def test_defaults(self):
+        _read_coords(self.rawtext, decimal = True) == _read_coords(self.rawtext)
+        _read_coords(self.rawtext, decimal = False) != _read_coords(self.rawtext)
+
+    def test_decimal(self):
+        observed = _read_coords(self.rawtext, decimal = True)
+        expected = [
+            # Latitude, longitude
+            (29.277288888888886, -89.62886388888889),
+            (29.297069444444446, -89.66436944444445),
+            (29.449427777777778, -89.603175),
+            (29.381333333333334, -89.57693055555555),
+            (29.351375, -89.50199444444445),
+            (29.20275277777778, -89.64348055555556),
+            (29.214583333333334, -89.61380555555554)
+            # &c.
+        ]
+        self.assertListEqual(observed, expected)
+
+    def test_positive(self):
+        observed = _convert_coords(29, 16, 38.24 )
+        self.assertAlmostEqual(observed, 29.27728888, delta = 10**10)
+
+    def test_negative(self):
+        observed = _convert_coords(-89, -37, -43.91 )
+        self.assertAlmostEquals(observed, 89.62886388888889, delta = 10**10)
+
+    def test_different_signs(self):
+        "Sending parameters of different signs raises a ValueError."
+        with self.assertRaises(ValueError):
+            _convert_coords(-89, 37, 43.91 )
+        with self.assertRaises(ValueError):
+            _convert_coords(0, -37, 43.91 )
+        _convert_coords(0, 37, 43.91 )
+        _convert_coords(0, 0, 43.91 )
+        _convert_coords(0, 0, -43.91 )
+
+class TestReadPublicNotice(unittest.TestCase):
+    def setUp(self):
+        rawtext = open(os.path.join(os.environ['WETLANDS_ROOT'], 'nose', 'fixtures', 'Kilbride PN.pdf.txt')).read()
+        self.data = read_public_notice(rawtext)
+    def test_keys(self):
+        observed = set(self.data.keys())
+        expected = {
+            'WQC',
+            'CUP',
+            'Acres',
+            'Parish',
+            'Coords',
+            'Basin',
+            'HUC',
+            '10 acre',
+
+            # Basic terms
+            'Mitigation Bank',
+            'Drill',
+            'Road',
+            'Section 10',
+            'Section 404',
+
+            'CUPException',
+            'WQCException',
+            'CoordsException',
+            'AcresException',
+            'TermsException',
+        }
+        self.assertSetEqual(observed, expected)
+    def test_types(self):
+        observed = dict(zip(
+            self.data.keys(),
+            [type(v) for v in self.data.values()]
+        ))
+        expected = {
+            'WQC': unicode,
+            'CUP': set,
+            'Mitigation Bank': bool,
+            'Acres': list,
+            'Parish': unicode,
+            'Coords': list,
+            'Basin': unicode,
+#           'HUC',
+            '10 acre': bool,
+            'Section 10': bool,
+            'Section 404': bool,
+        }
+
+        # Check types within lists
+        set(self.data['Acres']).issubset({float})
+        set(self.data['Coords']).issubset({tuple})
+
+        for c in self.data['Coords']:
+            # Latitude, longitude
+            len(c) == 2
+            type(c[0]) == type(c[1]) == float
+
+            # Within a reasonable boundary
+            29 < c[0] < 31
+            88 < c[1] < 94
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ v2.1/src/pdf.py
@@ -0,0 +1,214 @@
+# -*- encoding: utf-8 -*-
+import re
+
+def main():
+    import sys
+    # Read input
+    permit, papertype = sys.argv[1:]
+    text = sys.stdin.read()
+
+    # Parse
+    doc = read_public_notice(text)
+    doc['_id'] = permit
+    doc['permitApplicationNumber'] = permit
+
+    # Connect to database
+    import pymongo
+    connection = pymongo.Connection('localhost')
+    db = connection.wetlands
+
+    # Things based on paper type the collection
+    if papertype == 'public_notice':
+        doc['CUP'] = list(doc['CUP'])
+        collection = db.public_notice
+    elif papertype == 'drawings':
+        collection = db.drawings
+    else:
+        raise ValueError(
+            'Paper type must be one of "public_notice" or "drawings"; ' +
+            papertype + ' is not allowed.'
+        )
+
+    # Save
+    collection.insert(doc)
+
+    # Database column names (arg why did I use different ones...)
+    paperType = {
+        'public_notice': 'publicNotice',
+        'drawings': 'drawings',
+    }[papertype]
+
+    # Mark as processed
+    db.permit.update(
+        {"_id": permit},
+        {"$set": {paperType + ".processed": True }}
+    )
+
+def read_public_notice(rawtext):
+    "Get everything from the notice."
+    data = {}
+
+    try:
+        data['Acres'] = _read_acres(rawtext)
+    except Exception, e:
+        data['AcresException'] = unicode(type(e)) + ': ' + unicode(e)
+        data['Acres'] = []
+    else:
+        data['AcresException'] = None
+
+    try:
+        data.update(_read_terms(rawtext))
+    except Exception, e:
+        data['TermsException'] = unicode(type(e)) + ': ' + unicode(e)
+    else:
+        data['TermsException'] = None
+
+    try:
+        data['CUP'] = _read_cup_number(rawtext)
+    except Exception, e:
+        data['CUPException'] = unicode(type(e)) + ': ' + unicode(e)
+        data['CUP'] = u''
+    else:
+        data['CUPException'] = None
+
+    try:
+        data['WQC'] = _read_wqc_number(rawtext)
+    except Exception, e:
+        data['WQCException'] = unicode(type(e)) + ': ' + unicode(e)
+        data['WQC'] = u''
+    else:
+        data['WQCException'] = None
+
+    try:
+        data['Coords'] = _read_coords(rawtext)
+    except Exception, e:
+        data['CoordsException'] = unicode(type(e)) + ': ' + unicode(e)
+        data['Coords'] = []
+    else:
+        data['CoordsException'] = None
+
+    return data
+
+def strip_ws(rawtext):
+    return ''.join(rawtext.split())
+
+ACRES = re.compile(r'([0-9,.]+)acre')
+def _read_acres(rawtext):
+    raw = re.findall(ACRES, strip_ws(rawtext))
+    return [float(a.replace(',', '')) for a in raw]
+
+TERMS = [
+    "Mitigation Bank",
+    "Drill",
+    "Road",
+    "Section 10",
+    "Section 404"
+]
+CLEANTERMS = [t.lower().replace(' ', '') for t in TERMS]
+def _read_terms(rawtext):
+    rawtext = strip_ws(rawtext).lower()
+    return {term: (cleanterm in rawtext) for cleanterm, term in zip(CLEANTERMS, TERMS) }
+
+#           --> optionally, list numbers for "cubic yards"  or "material".  this is more difficult.  These will be numbers estimating the volumes of material dredged or filled.
+
+CUP_NUMBER = re.compile(r'\s(P\d{8})[^0-9]')
+def _read_cup_number(rawtext):
+    return set(re.findall(CUP_NUMBER, rawtext))
+
+WQC_NUMBER = re.compile(r'WQCApplicationNumber[^0-9]*([0-9-]+)')
+def _read_wqc_number(rawtext):
+    rawtext = strip_ws(rawtext)
+    wqc_numbers = re.findall(WQC_NUMBER, rawtext)
+
+    if len(wqc_numbers) > 1:
+        raise AssertionError('Multiple WQC numbers found')
+    elif len(wqc_numbers) == 0:
+        raise AssertionError('No WQC numbers found')
+
+    no_hyphen = wqc_numbers[0].replace('-', '')
+    if len(no_hyphen) != 8:
+        raise AssertionError('WQC number has the wrong length.')
+
+    return no_hyphen[:6] + '-' + no_hyphen[-2:]
+
+
+MINUTE_COORDS = re.compile(r'(-?\d+)°(\d+)\'([0-9.]+)"([NW])')
+DECIMAL_COORDS = re.compile(r'(lat|latitude|long|longitude)[0-9.]+',
+    flags = re.IGNORECASE)
+
+def _read_coords(rawtext, **kwargs):
+    "Get coordinates from the notice."
+    rawtext = strip_ws(rawtext)
+
+    rawcoords = re.findall(MINUTE_COORDS, rawtext)
+    if len(rawcoords) > 0:
+        return _clean_minute_coords(rawcoords, **kwargs)
+
+    rawcoordsd = re.findall(DECIMAL_COORDS, rawtext)
+    return _clean_minute_coords(rawcoordsd)
+
+def _clean_minute_coords(rawcoords, decimal = True, verbose = False):
+    cleancoords = []
+    while len(rawcoords) > 0:
+        first = rawcoords.pop(0)
+        second = rawcoords.pop(0)
+
+        f = first[-1]
+        s = second[-1]
+        if f != 'N':
+            raise ValueError('Wrong direction: %s' % f)
+        if s != 'W':
+            raise ValueError('Wrong direction: %s' % s)
+
+        # Handle sign
+        s = int(second[0])
+        if s < 0:
+            second = [-s] + list(second[1:3])
+
+        f = int(first[0])
+        s = int(second[0])
+        if not 28 < f < 32:
+            raise ValueError('Strange latitude: %d' % f)
+        if not 88 < s < 94:
+            raise ValueError('Strange longitude: %d' % s)
+
+
+        lat = tuple([float(f) for f in first[:3]])
+        lng = tuple([-float(s) for s in second[:3]])
+
+        if decimal:
+            latlng = tuple([_convert_coords(*foo) for foo in [lat, lng]])
+            cleancoords.append(latlng)
+        else:
+            cleancoords.append((lat, lng))
+
+        if verbose:
+            print lat, lng
+
+    return cleancoords
+
+def _convert_coords(degrees, minutes, seconds):
+    [degrees, minutes, seconds] = map(float, [degrees, minutes, seconds])
+
+    # Check signs
+    for arg in [degrees, minutes, seconds]:
+        if arg == 0:
+            continue
+        elif arg > 0:
+            positive = True
+            break
+        elif arg < 0:
+            positive = False
+            break
+        else:
+            raise ValueError('wtf?')
+    for arg in [degrees, minutes, seconds]:
+        if positive and arg < 0:
+            raise ValueError('All arguments must have the same sign.')
+        elif not positive and arg > 0:
+            raise ValueError('All arguments must have the same sign.')
+
+    return degrees + minutes/60 + seconds/3600
+
+if __name__ == '__main__':
+    main()
